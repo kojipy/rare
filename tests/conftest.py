713e98c06f4c30665ec6357f0b9f157c9d454831
sakamaki <ryu12kojiro@gmail.com>
2022-12-28 16:21:30 +0900

    feat:add testcase

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from omegaconf import OmegaConf
 
+from src.model.lang_model import LitLstmLM4Debug
+from src.model.rare import Rare
+
 
 @pytest.fixture
 def config():
@@ -8,3 +11,13 @@ def config():
     train_cfg = OmegaConf.load("tests/assets/config/train.yaml")
     cfg = OmegaConf.merge(model_cfg, train_cfg)
     return cfg
+
+
+@pytest.fixture
+def rare(config):
+    return Rare(config)
+
+
+@pytest.fixture
+def lang_model():
+    return LitLstmLM4Debug()
--- tests/test_model.py
+++ tests/test_model.py
@@ -10,3 +10,14 @@ def test_inference():
     rare = Rare(cfg).to("cuda")
     rare.eval()
     rare(image)
+
+
+def test_inference_lm(rare, lang_model):
+    rare = rare.to("cuda")
+    lang_model = lang_model.to("cuda")
+
+    rare.eval()
+    lang_model.eval()
+
+    img = torch.zeros(1, 3, 96, 2304).to("cuda")
+    _ = rare.predict_with_lm(img, lang_model)
