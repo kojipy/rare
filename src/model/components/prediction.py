c76a4d2f48ba7d7a53b3d71339a82b9f40797b07
sakamaki <ryu12kojiro@gmail.com>
2023-01-08 13:57:31 +0900

    format:comment

--- src/model/components/prediction.py
+++ src/model/components/prediction.py
@@ -23,9 +23,13 @@ class Attention(nn.Module):
     def forward(self, batch_H, text=None, batch_max_length=25):
         """
         input:
-            batch_H : contextual_feature H = hidden state of encoder. [batch_size x num_steps x contextual_feature_channels]
-            text : the text-index of each image. [batch_size x (max_length+1)]. +1 for [GO] token. text[:, 0] = [GO].
-        output: probability distribution at each step [batch_size x num_steps x num_classes]
+            batch_H : contextual_feature H = hidden state of encoder.
+                [batch_size x num_steps x contextual_feature_channels]
+            text : the text-index of each image.
+                [batch_size x (max_length+1)]. +1 for [GO] token. text[:, 0] = [GO].
+        output:
+            probability distribution at each step
+            [batch_size x num_steps x num_classes]
         """
         batch_size = batch_H.size(0)
         num_steps = batch_max_length + 1  # +1 for [s] at end of sentence.
